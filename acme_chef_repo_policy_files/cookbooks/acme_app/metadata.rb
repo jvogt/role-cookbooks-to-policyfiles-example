0c25057f1fa7a8353ad6a0f97d3fb74633813ad1
Jeff Vogt <jvogt@chef.io>
2019-05-16 12:58:54 -0700

    cleanup and updates

--- acme_chef_repo_policy_files/cookbooks/acme_app/metadata.rb
+++ acme_chef_repo_policy_files/cookbooks/acme_app/metadata.rb
@@ -7,7 +7,7 @@ long_description 'Installs/Configures acme_app'
 version '0.1.0'
 chef_version '>= 13.0'
 
-depends 'apache'
+depends 'apache2'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- acme_chef_repo_policy_files/cookbooks/acme_app/recipes/default.rb
+++ acme_chef_repo_policy_files/cookbooks/acme_app/recipes/default.rb
@@ -4,6 +4,8 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
+include_recipe 'apache2'
+
 directory node['apache']['docroot_dir'] do
   recursive true
 end
@@ -11,5 +13,3 @@ end
 file "#{node['apache']['docroot_dir']}/index.html" do
   content "Hello World from #{node['acme']['app_name']}!!\n"
 end
-
-include_recipe 'apache2'
